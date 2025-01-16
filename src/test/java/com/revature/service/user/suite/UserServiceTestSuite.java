42fd2d61e8a07707a379fb3ff736b950072ec73d
Saumya Puthenveettil <sputhen2000@gmail.com>
2025-01-16 17:52:45 -0500

    Add files via upload

--- src/test/java/com/revature/service/user/suite/UserServiceTestSuite.java
+++ src/test/java/com/revature/service/user/suite/UserServiceTestSuite.java
@@ -1,8 +1,9 @@
-package com.revature.service.user.suite;
+package com.revature.service.suite;
 
-import com.revature.service.user.negative.UserServiceAuthenticateNegativeTest;
-import com.revature.service.user.negative.UserServiceCreateUserNegativeTest;
-import com.revature.service.user.positive.UserServicePositiveTest;
+import com.revature.service.negative.Unit_RegisterDuplicateUsername;
+import com.revature.service.negative.UserServiceAuthenticateNegativeTest;
+import com.revature.service.negative.UserServiceCreateUserNegativeTest;
+import com.revature.service.positive.UserServicePositiveTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -10,7 +11,9 @@ import org.junit.runners.Suite;
 @Suite.SuiteClasses({
         UserServicePositiveTest.class,
         UserServiceCreateUserNegativeTest.class,
-        UserServiceAuthenticateNegativeTest.class
+       UserServiceAuthenticateNegativeTest.class,
+      //  Unit_RegisterDuplicateUsername.class
+
 })
 public class UserServiceTestSuite {
 }
