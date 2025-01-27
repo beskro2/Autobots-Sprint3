515d213a49dd7749eba8eadc39c025bd4bb5369d
nickxie280 <nicholas280@revature.net>
2025-01-27 11:01:39 -0500

    Defects fixed

--- src/main/java/com/revature/planetarium/repository/user/UserDaoImp.java
+++ src/main/java/com/revature/planetarium/repository/user/UserDaoImp.java
@@ -15,8 +15,9 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public Optional<User> createUser(User newUser) {
-        try (Connection conn = DatabaseConnector.getConnection(); 
-             PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)){
+        try (Connection conn = DatabaseConnector.getConnection();
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO users (username, password) VALUES (?, ?)", Statement.RETURN_GENERATED_KEYS)){
+            validation(newUser);
             stmt.setString(1, newUser.getUsername());
             stmt.setString(2, newUser.getPassword());
             stmt.executeUpdate();
@@ -31,6 +32,26 @@ public class UserDaoImp implements UserDao {
             throw new UserFail(e.getMessage());
         }
         return Optional.empty();
+    }
+
+    private void validation(User newUser) throws SQLException{
+        if (findUserByUsername(newUser.getUsername()).isPresent()){ // checks if the username exists
+            throw new SQLException("Invalid username");
+        }else if (newUser.getUsername().length() < 5 || newUser.getUsername().length() > 30){ // checks if the username meets the length constraint
+            throw new SQLException("Invalid username");
+        }else if (!newUser.getUsername().matches("^[a-zA-Z].*")){ // checks if the username starting char is any letter using regex (cap or lower)
+            throw new SQLException("Invalid username");
+        }else if (!newUser.getUsername().matches("^[a-zA-Z0-9_-]*$")) { // checks if the username supports lower case, upper case, numeric characters, underscores, and dashes
+            throw new SQLException("Invalid username");
+        }else if (newUser.getPassword().length() < 5 || newUser.getPassword().length() > 30) { // checks if the password meets the length constraint
+            throw new SQLException("Invalid password");
+        }else if (!newUser.getPassword().matches("^[a-zA-Z].*")){ // checks if the password starting char is any letter using regex (cap or lower)
+            throw new SQLException("Invalid password");
+        }else if (!newUser.getPassword().matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$")){ // checks if the password contains at least one lower case, uppercase and number
+            throw new SQLException("Invalid password");
+        }else if (!newUser.getPassword().matches("^[a-zA-Z0-9_-]*$")){ // checks if the password supports underscores and dashes
+            throw new SQLException("Invalid password");
+        }
     }
 
     @Override
