e5df9b66c99dfeca8eacacd048ce8779f36f8182
Ben Eskro <125532605+beskro2@users.noreply.github.com>
2025-01-27 15:53:37 -0600

    Update MoonDao.java

--- src/main/java/com/revature/planetarium/repository/moon/MoonDao.java
+++ src/main/java/com/revature/planetarium/repository/moon/MoonDao.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import com.revature.planetarium.entities.Moon;
 
 public interface MoonDao {
-
+//you got the right one
     Optional<Moon> createMoon(Moon moon);
     Optional<Moon> readMoon(int id);
     Optional<Moon> readMoon(String name);
